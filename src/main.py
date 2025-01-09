eedf9874ba818c633272c60c19440cf2854425cc
gijstemp <gijstempelman@gmail.com>
2025-01-09 13:40:00 +0000

    change textnode.py, define main()

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from textnode import *
+
+TEXT = "This is a text node"
+URL = "www.something.com"
+
+def main():
+    textnode = TextNode(TEXT, TextType.BOLD, URL)
+    print(textnode)
+
+
+if __name__ == "__main__":
+    main()
