0897881554965dcca01389d31483a601f7c28231
gijstemp <gijstempelman@gmail.com>
2025-01-09 14:53:14 +0000

    add hmtlnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,7 +7,7 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
-        node3 = TextNode("This is a text node", TextType.ITALIC)
+        node3 = TextNode("This is a text node", TextType.BOLD, "www.something.com")
         self.assertEqual(node, node2, node3)
 
 
